79147164e41658a85fc07f0fb6d248ae85c1d9fc
Chris Wiederspan <chris@wiederspan.com>
2012-09-20 12:55:46 -0600

    Initial project creation and commit
    Added the initial VS 2012 solution and projects.

---
+++ SourceMax.DataAccess.Fakes.Tests/FakeDbSetTests.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Data.Entity;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace SourceMax.DataAccess.Fakes.Tests {
+
+    [TestClass]
+    public class FakeDbSetTests {
+
+        [TestMethod]
+        public void GetType_Default_ImplementsIDbSet() {
+
+            // Arrange
+            var repository = new FakeDbSet<DummyModel>();
+
+            // Act
+            var type = repository.GetType();
+
+            // Assert
+            Assert.IsTrue(type.GetInterfaces().Any(x => x.IsGenericType && x.GetGenericTypeDefinition() == typeof(IDbSet<>)));
+
+            //repository.Add(new DummyModel { Id = 1, Value = "dummy item #1" });
+        }
+    }
+}
